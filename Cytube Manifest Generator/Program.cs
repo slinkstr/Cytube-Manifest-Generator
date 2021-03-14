38a5f15d6bab0a5ed6e95299f6ea08ae927dba36
slinky <49608195+slinkstr@users.noreply.github.com>
2021-03-13 23:47:30 -0800

    rewrite

--- Cytube Manifest Generator/Program.cs
+++ Cytube Manifest Generator/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
 using System.Linq;
 using MediaToolkit;
+using MediaToolkit.Model;
 using System.Text;
 using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Cytube_Manifest_Generator
 {
@@ -15,229 +17,194 @@ namespace Cytube_Manifest_Generator
     {
         static void Main(string[] args)
         {
-            // Check for no args (no files)
-            if (args.Length <= 0)
-            {
-                Console.WriteLine("Drag and drop files onto this exe to use.");
-                Console.ReadKey();
-                Environment.Exit(404);
-            }
+            // valid formats retrieved from https://github.com/calzoneman/sync/blob/3.0/docs/custom-media.md
+            List<string> supportedSourceTypes = new List<string>() { "mp4", "webm", "ogg", "aac", "ogg", "mpeg" };
+            List<string> supportedTextTypes = new List<string>() { "vtt" };
+            List<int> supportedQualityLevels = new List<int>() { 240, 360, 480, 540, 720, 1080, 1440, 2160 };
 
-            // Set up dictionary to store values from the foreach below
-            var dict = new Dictionary<string, FileInfo>();
-            string[] supportedVideo = { ".mp4", ".m4a", ".webm", ".ogg", ".aac" };
-            string[] supportedText = { ".vtt" };
-
-            // Iterate through every file provided and halt execution if one of them is invalid
-            foreach (var file in args)
-            {
-                // Add to dictionary if it's found to be valid
-                if (supportedVideo.Any(x => file.EndsWith(x)) || supportedText.Any(x => file.EndsWith(x)))
-                {
-                    dict[file] = new FileInfo(file);
-                }
-                // user provided invalid file
-                else
-                {
-                    string response = $"Invalid content type: {file}\nSupported formats: ";
-                    foreach (string format in supportedVideo)
-                    {
-                        response += format + " ";
-                    }
-                    foreach (string format in supportedText)
-                    {
-                        response += format + " ";
-                    }
-
-                    Console.WriteLine(response + "\nPress any key to exit");
-                    Console.ReadKey();
-                    Environment.Exit(404);
-                }
-            }
-
-            // Movie object to hold all the values
-            Movie movie = new Movie();
-
-            // Getting exe's current location and reading baseUrl.txt to set the baseUrl string
-            string path = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
-            var directory = System.IO.Path.GetDirectoryName(path).Substring(6);
-
-            // Reading file contents and removing any trailing slashes, to ensure there's only 1 slash
             IConfiguration config = BuildConfig();
             string baseUrl = config["baseUrl"].Trim('/') + "/";
 
-            // Iterate through all files with valid extension (text and video)
-            foreach (var info in dict)
+            if (args.Length < 1)
             {
-                if (supportedVideo.Any(x => info.Key.EndsWith(x)))
+                Console.WriteLine($"Drag and drop media files or a folder of media files onto {Process.GetCurrentProcess().ProcessName}.exe");
+                Exit();
+            }
+
+            // get all files inside a folder
+            string subfolder = "";
+            if (args.Length == 1)
+            {
+                var attr = File.GetAttributes(args[0]);
+                if (attr.HasFlag(FileAttributes.Directory))
                 {
-                    // Get a tuple with the width (1), height (2), length in ticks (3), and bitrate (4)
-                    var videoInfo = GetVideoInfo(info.Key);
+                    subfolder = args[0].Split('\\').Last() + "/";
+                    var fileList = Directory.GetFiles(args[0]);
+                    args = fileList;
+                }
+            }
 
-                    // Set title, duration, and it's never live
-                    movie.title = info.Value.Name;
-                    movie.duration = (long)TimeSpan.FromTicks(videoInfo.Item3).TotalSeconds;
-                    movie.live = false;
-
-                    // Setting url to the file
-                    movie.sources.url = $"{baseUrl}{info.Value.Name}";
-
-                    // Check the first three formats and insert "video/", check the last 2 and insert "audio/"
-                    if (supportedVideo.Take(3).Any(x => info.Value.Extension == x))
+            var allSupportedTypes = supportedSourceTypes.Concat(supportedTextTypes);
+            bool unsupportedType = false;
+            List<Source> sources = new List<Source>();
+            List<TextTrack> textTracks = new List<TextTrack>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                // unsupported file format
+                if (!allSupportedTypes.Any(x => args[i].EndsWith(x)))
+                {
+                    Console.WriteLine($"Unsupported file type: {args[i]}");
+                    unsupportedType = true;
+                }
+                // text tracks
+                else if (supportedTextTypes.Any(x => args[i].EndsWith(x)))
+                {
+                    string filename = args[i].Split('\\').Last();
+                    string extension = filename.Split('.').Last();
+                    TextTrack tt = new TextTrack()
                     {
-                        movie.sources.contentType = "video/" + info.Value.Extension.Substring(1, info.Value.Extension.Length - 1);
-                    }
-                    else if (supportedVideo.TakeLast(2).Any(x => info.Value.Extension == x))
+                        url = baseUrl + subfolder + filename,
+                        contentType = "text/" + extension,
+                        name = "Subtitles " + i,
+                        isDefault = false
+                    };
+                    textTracks.Add(tt);
+                }
+                // video/audio tracks
+                else
+                {
+                    string filename = args[i].Split('\\').Last();
+                    string extension = filename.Split('.').Last();
+                    Metadata md = GetVideoInfo(args[i]);
+                    int quality = supportedQualityLevels.First();
+                    for (int j = 0; j < supportedQualityLevels.Count; j++)
                     {
-                        movie.sources.contentType = "audio/" + info.Value.Extension.Substring(1, info.Value.Extension.Length);
-                    }
-
-                    // All valid qualities from the cytube custom media page
-                    int[] validSizes = new int[] { 240, 360, 480, 540, 720, 1080, 1440, 2160 };
-
-                    movie.sources.quality = 240;
-
-                    // Setting quality based on just the width of the video, rounded up
-                    for (int i = 0; i < validSizes.Length; i++)
-                    {
-                        // Work up validSizes, rounding up to the nearest size
-                        if (validSizes[i] >= videoInfo.Item1)
+                        int frameHeight = int.Parse(md.VideoData.FrameSize.Split('x')[1]);
+                        if (frameHeight >= supportedQualityLevels[j])
                         {
-                            movie.sources.quality = validSizes[i];
-                            break;
+                            quality = supportedQualityLevels[j];
                         }
                     }
 
-                    // Setting bitrate
-                    movie.sources.bitrate = videoInfo.Item4;
-                }
+                    int totalBitrate = (md.VideoData.BitRateKbs ?? 0) + md.AudioData.BitRateKbs;
 
-                // Handling subtitle tracks
-                else if (supportedText.Any(x => info.Key.EndsWith(x)))
-                {
-                    movie.textTracks.url = $"{baseUrl}{info.Value.Name}";
-                    movie.textTracks.contentType = "text/vtt";
-                    movie.textTracks.name = "English Subtitles";
-                }
-
-                // I genuinely have no idea how someone could ever end up here
-                else
-                {
-                    Console.WriteLine("if you see this message copy and paste output to the developer");
+                    Source s = new Source()
+                    {
+                        url = baseUrl + subfolder + filename,
+                        contentType = "video/" + extension,
+                        quality = quality,
+                        bitrate = totalBitrate,
+                        duration = Convert.ToInt32(md.Duration.TotalSeconds)
+                    };
+                    sources.Add(s);
                 }
             }
-            // Print out everything in the console window
-            Console.WriteLine(movie.ToString());
+            if (unsupportedType)
+            {
+                Console.WriteLine("Valid file types: " + string.Join(", ", allSupportedTypes));
+                Exit();
+            }
+            // only text tracks provided
+            if (sources.Count < 1)
+            {
+                Console.WriteLine("No source files provided.");
+                Console.WriteLine("Valid source file types: " + string.Join(", ", supportedSourceTypes));
+                Exit();
+            }
+
+            string title = sources.First().url.Split('/').Last().Split('.').First();
+
+            JObject json = new JObject(
+                new JProperty("title", title),
+                new JProperty("duration", sources.First().duration),
+                new JProperty("live", false),
+                new JProperty("sources",
+                    new JArray(
+                        from s in sources
+                        select new JObject(
+                            new JProperty("url", s.url),
+                            new JProperty("contentType", s.contentType),
+                            new JProperty("quality", s.quality),
+                            new JProperty("bitrate", s.bitrate)
+                        )
+                    )
+                ),
+                new JProperty("textTracks",
+                    new JArray(
+                        from t in textTracks
+                        select new JObject(
+                            new JProperty("url", t.url),
+                            new JProperty("contentType", t.contentType),
+                            new JProperty("name", t.name),
+                            new JProperty("default", t.isDefault)
+                        )
+                    )
+                )
+            );
+
+            File.WriteAllText(subfolder + json["title"] + ".json", json.ToString());
+
+            // By default, browsers block requests for WebVTT tracks hosted on different domains than the current page. 
+            // In order for text tracks to work cross-origin, the Access-Control-Allow-Origin header needs to be set by the remote server when serving the VTT file.
+            if (textTracks.Count > 0)
+            {
+                File.WriteAllText(subfolder + ".htaccess", "Header set Access-Control-Allow-Origin \"*\"");
+            }
+        }
+
+        public static void Exit()
+        {
             Console.WriteLine();
-
-            // Serialize the movie object to JSON
-            string json = JsonConvert.SerializeObject(movie, Formatting.Indented);
-
-            // Get the directory of any source (really) to put the JSON in
-            var firstSource = dict.First();
-            var firstDirectory = firstSource.Value.DirectoryName;
-            var jsonDestination = firstDirectory + "\\" + firstSource.Value.Name.Substring(0, firstSource.Value.Name.Length - firstSource.Value.Extension.Length) + ".json";
-
-
-            // Create the file
-            FileStream fs = new FileStream(jsonDestination, FileMode.Create);
-
-            // Convert json text to bytes to write it into this file
-            byte[] bdata = Encoding.Default.GetBytes(json);
-            fs.Write(bdata, 0, bdata.Length);
-            fs.Close();
-
-            // Final confirmation
-            Console.WriteLine("File has been created in " + firstDirectory);
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
-        }
-
-        // Objects to serialize into a json later on
-        public class Movie
-        {
-            public string title { get; set; }
-            public long duration { get; set; }
-            public bool live { get; set; }
-            
-            // Named confusingly, text sources have a separate place
-            public VideoSource sources { get; set; }
-            public TextSource textTracks { get; set; }
-
-            public Movie()
-            {
-                title = "";
-                duration = 0;
-                live = false;
-                sources = new VideoSource();
-                textTracks = new TextSource();
-            }
-
-            public override string ToString()
-            {
-                string returnvalue = $"Title: {title}\n" +
-                    $"Duration: {duration}\n" +
-                    $"Live: {live}\n" +
-                    $"Video\n" +
-                    $"    URL: {sources.url}\n" +
-                    $"    ContentType: {sources.contentType}\n" +
-                    $"    Quality: {sources.quality}\n" +
-                    $"    Bitrate: {sources.bitrate}\n" +
-                    $"Subtitles\n" +
-                    $"    URL: {textTracks.url}\n" +
-                    $"    Name: {textTracks.name}\n" +
-                    $"    ContentType: {textTracks.contentType}";
-
-                return returnvalue;
-            }
-        }
-
-        public class VideoSource
-        {
-            public string url { get; set; }
-            public string contentType { get; set; }
-            public int quality { get; set; }
-            public int bitrate { get; set; }
-        }
-
-        public class TextSource
-        {
-            public string url { get; set; }
-            public string contentType { get; set; }
-            public string name { get; set; }
+            Environment.Exit(0);
         }
 
         // a method to get Width, Height, Duration in Ticks, and bitrate for a video
-        public static Tuple<int, int, long, int> GetVideoInfo(string fileName)
+        public static Metadata GetVideoInfo(string fileName)
         {
-            var inputFile = new MediaToolkit.Model.MediaFile { Filename = fileName };
+            var inputFile = new MediaFile { Filename = fileName };
 
             using (var engine = new Engine())
             {
                 engine.GetMetadata(inputFile);
             }
 
-            // FrameSize is returned as '1280x768' string.
-            var size = inputFile.Metadata.VideoData.FrameSize.Split(new[] { 'x' }).Select(o => int.Parse(o)).ToArray();
-            int bitrate = inputFile.Metadata.VideoData.BitRateKbs ?? int.MaxValue;
+            return inputFile.Metadata;
+        }
 
-            return new Tuple<int, int, long, int>(size[0], size[1], inputFile.Metadata.Duration.Ticks, bitrate);
+        public class Source
+        {
+            public string url { get; set; }
+            public string contentType { get; set; }
+            public int quality { get; set; }
+            public int bitrate { get; set; }
+            public long duration { get; set; }
+        }
+
+        public class TextTrack
+        {
+            public string url { get; set; }
+            public string contentType { get; set; }
+            public string name { get; set; }
+            public bool isDefault { get; set; }
         }
 
         // Read the config file
         private static IConfiguration BuildConfig()
         {
             // this whole thing is very questionable.
+            // All these give me the temp file the single-file app created.
             // - System.Reflection.Assembly.GetEntryAssembly().Location 
             // - AppDomain.CurrentDomain.BaseDirectory
             // - Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
             // - System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
             // - System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
-            // All would give me the temp file (that doesn't exist) the single-file app created.
+            // These give me the path of the media files, not the exe.
             // - System.IO.Directory.GetCurrentDirectory()
             // - Environment.CurrentDirectory
-            // Would give me the path of the media files, not the exe.
+            // This outputs "Unhandled exception. System.ArgumentException: The path must be absolute. (Parameter 'root')"
             // - System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)
-            // outputs "Unhandled exception. System.ArgumentException: The path must be absolute. (Parameter 'root')"
             //
             // In the end I had to use System.Diagnostics and Process.GetCurrentProcess() to get the path of the REAL exe,
             // and then some finagling to remove the name of the exe.
--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Cytube-Manifest-Generator
-Create the manifest .json files needed to watch videos on cytube with subtitles
+Creates a custom media manifest for CyTube
 
 # How to Use
-Enter your base URL (https://example.com/movies) in **`config.json`**.
-Drag and drop your video and optionally subtitle track onto **`Cytube Manifest Generator.exe`**
-[MovieTitle].json will be created in the same folder as the video track.
+Enter your base URL in **`config.json`**.  
+Drag and drop your media files onto **`Cytube Manifest Generator.exe`**  
+The .json will be created in the same folder as the video track.
+
+If you drag and drop a folder with media files inside, the folder name will be included in the URLs.  
+e.g. https://example.com/baseurl/folder_that_I_dragged/videofile.mp4
+
+Otheriwse, only the base URL is used, e.g. https://example.com/baseurl/videofile.mp4
+
+# Supported file formats
+mp4, webm, ogg, aac, ogg, mpeg, vtt
+
+# Notes
+> By default, browsers block requests for WebVTT tracks hosted on different domains than the current page. In order for text tracks to work cross-origin, the `Access-Control-Allow-Origin` header needs to be set by the remote server when serving the VTT file.
+
+A .htaccess file is created to set this header for Apache users if you use a .vtt track. If you're using Nginx you will need to edit your config to set this header instead.
